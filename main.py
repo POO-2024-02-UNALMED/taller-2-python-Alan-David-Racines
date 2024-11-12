87c7d41e4ad71c5712c808b92a27af5eccb68391
Alan-David-Racines <aracines@unal.edu.co>
2024-11-12 00:00:21 +0000

    guardado 6

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 class Asiento:
+    colores = ["rojo", "verde", "amarillo", "negro", "blanco"]
+    
     def __init__(self, color: str, precio: int, registro: int):
         self.color = color
         self.precio = precio
         self.registro = registro
 
     def cambiarColor(self, color: str):
-        colores = ["rojo", "verde", "amarillo", "negro", "blanco"]
         if color in Asiento.colores:
             self.color = color
 
